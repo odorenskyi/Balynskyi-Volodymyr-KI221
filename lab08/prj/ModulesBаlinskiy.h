b5f6125124da2adef65d630891266a5d9d95980e
Volodymyr Balynskyi <80583171+VovaBoss20005@users.noreply.github.com>
2023-04-10 21:57:53 +0300

    Update ModulesBаlinskiy.h

--- lab08/prj/ModulesBаlinskiy.h
+++ lab08/prj/ModulesBаlinskiy.h
@@ -1,6 +1,9 @@
-#ifndef MODULESBALINSKIY_H
-#define MODULESBALINSKIY_H
+#define MATH_DEFINES
+#include <cmath>
 
-float s_calculation(float x, float y, float z);
+float s_calculation(x, y, z)
+{
+   S = pow((3*sin(sqrt(12*x+log10(x-3)))),y)+z/x;
+    return S;
+}
 
-#endif
